f1fdf7a0ace94310a584af64cbadb06249398f92
Ricardo Lora <82956080+RicardoLora313@users.noreply.github.com>
2023-11-15 10:01:20 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,6 +19,21 @@ db_config = {
 
 
 con = mysql.connector.connect(**db_config)
+def validar_nome(data):
+    nome = data.get("nome")
+    if not nome or not isinstance(nome, str) or not 3 <= len(nome) <= 50:
+     return False, "O campo 'nome' é obrigatório e deve ter entre 3 e 50 caracteres."
+
+def validar_idade(data):
+    idade = data.get("idade")
+    if not idade or not isinstance(idade, int) or idade <= 0:
+     return False, "O campo 'idade' é obrigatório e deve ser um número inteiro positivo."
+
+def validar_sexo(data):
+    sexo = data.get("sexo")
+    if not sexo or not isinstance(sexo, str) or sexo not in ("M", "F"):
+     return False, "O campo 'sexo' é obrigatório e deve ser 'M' (masculino) ou 'F' (feminino)."
+    
 
 def validar_dados(data):
     nome = data.get("nome")
@@ -108,12 +123,27 @@ def alterar_cadastro_existente(id):
         nome = data.get("nome")
         idade = data.get("idade")
         sexo = data.get("sexo")
-
-        query_update = "UPDATE pessoas SET nome = %s, idade = %s, sexo = %s WHERE id = %s"
-        values_update = (nome, idade, sexo, id)
-        execute_query(query_update, values_update)
-
-        return jsonify({"mensagem": "Cadastro atualizado com sucesso!"}), 200
+        if "nome" in data and data["nome"]:
+         validar_nome(data)
+         query_update = "UPDATE pessoas SET nome = %s WHERE id = %s"                                             #, idade = %s, sexo = %s WHERE id = %s
+         values_update = (nome, id)
+         execute_query(query_update, values_update)
+         return jsonify({"mensagem": "Nome atualizado com sucesso!"}), 200
+        
+        if "idade" in data and data["nome"]:
+         validar_idade(data)
+         query_update = "UPDATE pessoas SET idade = %s WHERE id = %s"                                             #, idade = %s, sexo = %s WHERE id = %s
+         values_update = (idade, id)
+         execute_query(query_update, values_update)
+         return jsonify({"mensagem": "Idade atualizada com sucesso!"}), 200
+        
+        if "sexo" in data and data["nome"]:
+         validar_sexo(data)
+         query_update = "UPDATE pessoas SET sexo = %s WHERE id = %s"                                             #, idade = %s, sexo = %s WHERE id = %s
+         values_update = (sexo, id)
+         execute_query(query_update, values_update)
+         return jsonify({"mensagem": "Sexo atualizado com sucesso!"}), 200
+        
     else:
         return jsonify({"erro": "A solicitação deve conter dados JSON"}), 400
 
